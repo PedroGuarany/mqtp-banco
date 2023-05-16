70fa8aec294be902b8ffbe2cc68e17549c5b6f11
Ismael Araújo <89144545+ismagoes@users.noreply.github.com>
2023-05-15 22:24:36 -0300

    feat: portas criadas, express configurado

---
+++ extrato/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(require("cors")());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const router = express.Router();
+router.get("/", (req, res) => res.sendFile("/index.html", { root: __dirname }));
+
+app.use("/", router);
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
