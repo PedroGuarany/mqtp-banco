90ffd4d93dda063b1ae4f44e084e234a9fb72594
Ismael Araújo <89144545+ismagoes@users.noreply.github.com>
2023-05-16 22:51:58 -0300

    rabbitmq+docker

--- conta/index.js
+++ conta/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+var amqp = require("amqplib/callback_api");
+
 const app = express();
 const port = 3001;
 
@@ -12,4 +14,30 @@ router.get("/", (req, res) => res.sendFile("/index.html", { root: __dirname }));
 app.use("/", router);
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
+amqp.connect("amqp://user:password@localhost", (error, connection) => {
+  if (error) {
+    throw error;
+  }
+
+  connection.createChannel((error1, channel) => {
+    if (error1) {
+      throw error1;
+    }
+    const queue = "cpf_isma";
+    const msg = {
+      cpf: "12345678910",
+      nome: "Ismael",
+      idade: 20,
+      valor: 1000,
+    };
+
+    channel.assertQueue(queue, {
+      durable: false,
+    });
+
+    channel.sendToQueue(queue, Buffer.from(JSON.stringify(msg)));
+    console.log(" [x] Sent %s", msg);
+  });
 });
