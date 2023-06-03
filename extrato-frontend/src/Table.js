0c73a9da1a8c48c5eb1183c94fdfb0fad95b9cb8
mwises <88902469+mwises@users.noreply.github.com>
2023-06-03 16:50:14 -0300

    fix: color do valor muda dependo do tipo

--- extrato-frontend/src/Table.js
+++ extrato-frontend/src/Table.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import './App.css';
 import { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
@@ -8,19 +9,18 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import TableRowComponent from './TableRowComponent';
 
 const dados = [
-    { id: 1, nome: 'Elias', valor: '522,00', data: '20/03/2023' },
-    { id: 2, nome: 'João', valor: '789,50', data: '15/04/2023' },
-    { id: 3, nome: 'Maria', valor: '125,75', data: '10/05/2023' },
-    { id: 4, nome: 'Ana', valor: '432,90', data: '03/06/2023' },
-    { id: 5, nome: 'Pedro', valor: '237,20', data: '27/06/2023' },
-    { id: 6, nome: 'Carla', valor: '632,15', data: '19/07/2023' },
-    { id: 7, nome: 'Mariana', valor: '198,40', data: '12/08/2023' },
-    { id: 8, nome: 'Lucas', valor: '421,80', data: '05/09/2023' },
-    { id: 9, nome: 'Sara', valor: '317,60', data: '28/09/2023' },
-    { id: 10, nome: 'Rafael', valor: '689,00', data: '21/10/2023' }
+    { id: 1, nome: 'Elias', tipo: 'transferenciaRecebida', valor: '522.00', data: '20/03/2023' },
+    { id: 2, nome: 'João', tipo: 'deposito', valor: '789.50', data: '15/04/2023' },
+    { id: 3, nome: 'Maria', tipo: 'deposito', valor: '125.75', data: '10/05/2023' },
+    { id: 4, nome: 'Ana', tipo: 'transferenciaRecebida', valor: '432.90', data: '03/06/2023' },
+    { id: 5, nome: 'Pedro', tipo: 'deposito', valor: '237.20', data: '27/06/2023' },
+    { id: 6, nome: 'Carla', tipo: 'deposito', valor: '632.15', data: '19/07/2023' },
+    { id: 7, nome: 'Mariana', tipo: 'deposito', valor: '198.40', data: '12/08/2023' },
+    { id: 8, nome: 'Lucas', tipo: 'deposito', valor: '421.80', data: '05/09/2023' },
+    { id: 9, nome: 'Sara', tipo: 'transferenciaRecebida', valor: '317.60', data: '28/09/2023' },
+    { id: 10, nome: 'Rafael', tipo: 'transferenciaRecebida', valor: '689.00', data: '21/10/2023' }
   ];
 
   
@@ -40,25 +40,33 @@ const dados = [
             alignItems: 'center',
             flexDirection: 'column',
             margin: '-127px auto',
-            fontFamily: 'Roboto, sans-serif'
+            fontFamily: 'Roboto, sans-serif',
+            boxShadow: 'rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px'
         }}>
             <TableContainer component={Paper}>
                 <Table sx={{ width: '100%' }} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell align="left">Nome</TableCell>
-                        <TableCell align="left">Valor</TableCell>
-                        <TableCell align="left">Data</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="left">Nome</TableCell>
+                            <TableCell align="left">Valor</TableCell>
+                            <TableCell align="left">Data</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
 
-                    {tableData.map(dadosDaTable => (
-                                <TableRowComponent key={dadosDaTable.id} nome={dadosDaTable.nome} valor={dadosDaTable.valor} data={dadosDaTable.data} />
-                            ))}
-            </TableBody>
-      </Table>
-    </TableContainer>
+                        {tableData.map(dadosDaTable => (
+                                    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }} key={dadosDaTable.id}>
+                                        <TableCell>{dadosDaTable.nome}</TableCell>
+                                        <TableCell className={dadosDaTable.tipo}>{new Intl.NumberFormat("pt-BR", {
+                                            style: "currency",
+                                            currency: "BRL"
+                                        }).format(dadosDaTable.valor)}</TableCell>
+                                        <TableCell>{dadosDaTable.data}</TableCell>
+                                    </TableRow>
+                                ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </Box>
     );
 }
